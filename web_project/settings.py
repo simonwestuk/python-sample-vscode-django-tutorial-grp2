a11e3c33f9cf3adcd9fdf68cc8691c85d1b34efd
Admin <simon@simon.com>
2024-02-15 11:29:56 +0000

    Added allowed hosts removed proto

--- web_project/settings.py
+++ web_project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost:8000',
     '127.0.0.1:8000',
-    'https://django-grp2.onrender.com',
+    'django-grp2.onrender.com',
 ]
 
 # Application definition
