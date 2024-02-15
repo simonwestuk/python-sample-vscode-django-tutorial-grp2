1095c31972a4056011530167365e8c6a0004fe57
Admin <simon@simon.com>
2024-02-15 11:28:49 +0000

    Added allowed hosts

--- web_project/settings.py
+++ web_project/settings.py
@@ -29,9 +29,11 @@ DEBUG = True
 # a specific host, you must also add 'localhost' and/or '127.0.0.1' for local
 # debugging (which are enabled by default when ALLOWED_HOSTS is empty.)
 ALLOWED_HOSTS = [
-    #'localhost',
-    #'127.0.0.1'
-    #'vscode-django-tutorial.azurewebsites.net'  # Sample name only!
+    'localhost',
+    '127.0.0.1',
+    'localhost:8000',
+    '127.0.0.1:8000',
+    'https://django-grp2.onrender.com',
 ]
 
 # Application definition
